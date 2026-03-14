bb8b8add36a54fa12d6ee4f84c3652d8fe3d1bb8
killdrago <killdrago@hotmail.com>
2026-03-14 07:49:19 +0100

    Update __init__.py

--- micheline/intel/__init__.py
+++ micheline/intel/__init__.py
@@ -1,17 +1,17 @@
 # micheline/intel/__init__.py
-"""
-Module Intel.
 
-Important:
-- On n'importe PAS automatiquement watchers ici.
-  Ça évite les erreurs d'import (RawEventsDB manquant, circular imports, etc.).
-- Pour utiliser le watcher:
-    from micheline.intel.watchers import WatcherService
-"""
-
-from .entity_registry import EntityRegistry, seed_default_entities
+from micheline.intel.entity_registry import EntityRegistry, seed_default_entities
+from micheline.intel.watchers import WatcherService, RawEventsDB, RobotsChecker, RateLimiter, Watcher
+from micheline.intel.event_cards import EventCardsDB, EventCardNormalizer
 
 __all__ = [
     "EntityRegistry",
     "seed_default_entities",
+    "WatcherService",
+    "RawEventsDB",
+    "RobotsChecker",
+    "RateLimiter",
+    "Watcher",
+    "EventCardsDB",
+    "EventCardNormalizer",
 ]
